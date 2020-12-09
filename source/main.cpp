02d613067753374b95a2bbd8772e909128f35fd4
birdbraintech <tlauwers@birdbraintechnologies.com>
2020-12-09 10:00:16 -0500

    Added some basic BLE updates to main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,35 @@
 #include "MicroBit.h"
 
 MicroBit uBit;
+BLEDevice BirdBrainBLE;
+NVMController mem_controller;
+MicroBitStorage storeMe(mem_controller, -1);
+
 
 int 
 main()
 {
+    bool advertising = true;
     uBit.init();
 
-    while(1)
-        uBit.display.scroll("HELLO WORLD!");
+    BirdBrainBLE.init("MB9C99D", uBit.getSerial(), uBit.messageBus, storeMe, true);
+    BirdBrainBLE.advertise();
+    BirdBrainBLE.setTransmitPower(7);
+
+    while(1) {
+        if (uBit.buttonA.isPressed()) {
+            if(advertising) {
+                advertising = false;
+                BirdBrainBLE.stopAdvertising();
+                uBit.display.image.print('X');
+                uBit.sleep(200);
+            }
+            else {
+                advertising = true;
+                BirdBrainBLE.advertise();
+                uBit.display.image.print('A');
+                uBit.sleep(200);
+            }
+        }
+    }
 }
